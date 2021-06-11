f888cba434ba2739bbc26803db3bf3e453038815
Paul Liboon <proliboon@gmail.com>
2021-06-11 23:33:38 +0800

    Added basic functionality of VCB user input.

--- core.rb
+++ core.rb
@@ -48,7 +48,6 @@ module TestPlugin
     end
 
     def onMouseMove (flags, x, y, view)
-
       # display cursor coordinates on status text bar
       # as mouse is hovering over the view port
       @cursor.pick(view, x, y)
@@ -56,19 +55,20 @@ module TestPlugin
       Sketchup.status_text = "#{@x}, #{@y}"
 
       view.invalidate
-
     end # onMouseMove method
 
     # Define the first and second points on click that will calculate
     # the positions of the other two points to make a rectangle
     def onLButtonDown (flags, x, y, view)
 
-      if @first_point.to_a.length > 0
+      if @first_point.length > 0
+
         @second_point = [@x.round(2), @y.round(2)]
         @pt2 = @second_point
         puts "2nd point: #{@pt2}"
         @first_point.clear
-        draw_square
+
+        draw_square(@pt1, @pt2)
       else
         @first_point = [@x.round(2), @y.round(2)]
         @pt1 = [@x.round(2), @y.round(2)]
@@ -87,10 +87,18 @@ module TestPlugin
     end
 
     def onUserText (text, view)
-      @arr = text.split(',',2)
-      @side_x, @side_y = @arr
+      sides = text.to_f
 
+      # If ONLY the first point is set by mouse click, we draw a square
+      # using the value on the VCB user input for all sides
+      if @first_point.length > 0 && @second_point.length == 0
 
+        pt2 = [ @pt1[0]+sides, @pt1[1]+sides ]
+        puts "@pt1 value: #{@pt1}"
+        puts "pt2 value: #{pt2}"
+
+        draw_square(@pt1, pt2)
+      end
 
       puts "VCB value: #{@sides}"
 
@@ -104,17 +112,18 @@ module TestPlugin
     # based on the dimensions provided by either
     # functions: setting 2 points or user input values
     # for two corners by typing in the Value Content Box
-    def draw_square
+    def draw_square(first, second)
 
-        @pt3 = [ @pt1.x, @pt2.y ]
-        @pt4 = [ @pt2.x, @pt1.y ]
+      pt1, pt2 = first, second
+      pt3 = [ first.x, second.y ]
+      pt4 = [ second.x, first.y ]
 
-        #draw face from 4 points and pull it back by 20
-        a_face = Sketchup.active_model.active_entities.add_face(@pt1, @pt3, @pt2, @pt4)
-        a_face.pushpull(-20, true)
+      #draw face from 4 points and pull it back by 20
+      a_face = Sketchup.active_model.active_entities.add_face(pt1, pt3, pt2, pt4)
+      a_face.pushpull(-20, true)
 
-        reset
-        view.invalidate
+      reset
+      view.invalidate
 
 
     end # draw_square method
@@ -122,7 +131,7 @@ module TestPlugin
     # Reset values of instance variables after every
     #successful drawing of a rectangle/square
     def reset
-      @pt1, @pt3, @pt2, @pt4 = nil
+      @pt1, @pt2 = nil
       @first_point.clear
       @second_point.clear
     end
